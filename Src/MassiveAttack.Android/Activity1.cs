918796e21012e6ded49b8a5548478328c4b1fb3f
Jarred Capellman <jcapellman@hotmail.com>
2016-11-20 13:44:55 -0600

    Added Common Library and WebAPI Service

--- Src/MassiveAttack.Android/Activity1.cs
+++ Src/MassiveAttack.Android/Activity1.cs
@@ -9,7 +9,6 @@ namespace MassiveAttack.Android {
         , Icon = "@drawable/icon"
         , Theme = "@style/Theme.Splash"
         , AlwaysRetainTaskState = true
-        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
         , ScreenOrientation = ScreenOrientation.SensorLandscape
         , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
     public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity {
