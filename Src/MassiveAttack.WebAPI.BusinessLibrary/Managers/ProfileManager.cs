172b18449a801f8c6ce267e926a58d69aa840d0d
Jarred Capellman <jcapellman@hotmail.com>
2016-11-26 23:24:01 -0600

    Added initial Profile functionality

---
+++ Src/MassiveAttack.WebAPI.BusinessLibrary/Managers/ProfileManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using MassiveAttack.Common.Library.Objects.Common;
+using MassiveAttack.Common.Library.Objects.WebAPI.Internal;
+
+namespace MassiveAttack.WebAPI.BusinessLibrary.Managers {
+    public class ProfileManager : BaseManager {
+        public ProfileManager(ControllerHandlerItem controllerHandlerItem) : base(controllerHandlerItem) { }
+
+        public async Task<ReturnSet<string>> GetProfile(Guid PlayerGUID) {
+
+        }
+    }
+}
---
+++ Src/MassiveAttack.WebAPI/Controllers/ProfileController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using MassiveAttack.Common.Library.Objects.Common;
+
+namespace MassiveAttack.WebAPI.Controllers {
+    public class ProfileController : BaseController {
+        public async Task<ReturnSet<string>> GET(Guid PlayerGUID)
+            => await ProfileManager(ControllerHandlerItem).GetProfile(PlayerGUID);
+    }
+}
