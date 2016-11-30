1ff45c3bd435e3ab1d873e98bdf9a55ff123d892
Jarred Capellman <jcapellman@hotmail.com>
2016-11-29 18:55:44 -0600

    Added Redis Wrappers in the BaseManager class to keep the redis code clean

--- Src/MassiveAttack.WebAPI.BusinessLibrary/Managers/BaseManager.cs
+++ Src/MassiveAttack.WebAPI.BusinessLibrary/Managers/BaseManager.cs
@@ -1,14 +1,36 @@
-﻿using MassiveAttack.Common.Library.Objects.WebAPI.Internal;
+﻿using System;
+using System.Threading.Tasks;
+
+using MassiveAttack.Common.Library.Objects.Common;
+using MassiveAttack.Common.Library.Objects.WebAPI.Internal;
+
+using MassiveAttack.WebAPI.DataLibrary.RModel;
 
 namespace MassiveAttack.WebAPI.BusinessLibrary.Managers {
-    public class BaseManager {
+    public abstract class BaseManager {
         private readonly ControllerHandlerItem _controllerHandlerItem;
+
+        public abstract string GetRedisPrefix();
+
+        public string GetRedisKey(Guid objectGuid) => $"{GetRedisPrefix()}_{objectGuid}";
 
         protected string SQLServerConnectionString => _controllerHandlerItem.SQLServerConnectionString;
 
         protected string RedisConnectionString => _controllerHandlerItem.RedisConnectionString;
-        
-        public BaseManager(ControllerHandlerItem controllerHandlerItem) {
+
+        protected async Task<ReturnSet<T>> GetRedisObject<T>(Guid objectGUID) {
+            using (var rModel = new RedisModel(RedisConnectionString)) {
+                return await rModel.Get<T>(GetRedisKey(objectGUID));
+            }
+        }
+
+        protected async Task<ReturnSet<bool>> WriteRedisObject<T>(Guid objectGUID, T objectValue) {
+            using (var rModel = new RedisModel(RedisConnectionString)) {
+                return await rModel.Write(GetRedisKey(objectGUID), objectValue);
+            }
+        }
+
+        protected BaseManager(ControllerHandlerItem controllerHandlerItem) {
             _controllerHandlerItem = controllerHandlerItem;
         }
     }
--- Src/MassiveAttack.WebAPI.BusinessLibrary/Managers/ProfileManager.cs
+++ Src/MassiveAttack.WebAPI.BusinessLibrary/Managers/ProfileManager.cs
@@ -6,41 +6,38 @@ using MassiveAttack.Common.Library.Objects.WebAPI.Internal;
 using MassiveAttack.Common.Library.Objects.WebAPI.Profiles;
 
 using MassiveAttack.WebAPI.DataLibrary.EFModel;
-using MassiveAttack.WebAPI.DataLibrary.RModel;
 
 using Microsoft.EntityFrameworkCore;
 
 namespace MassiveAttack.WebAPI.BusinessLibrary.Managers {
     public class ProfileManager : BaseManager {
         public ProfileManager(ControllerHandlerItem controllerHandlerItem) : base(controllerHandlerItem) { }
-
-        private string R_KEY = "PROFILE_";
-
+        
         public async Task<ReturnSet<ProfileResponseItem>> GetProfile(Guid PlayerGUID) {
-            using (var profileModel = new ProfileModel(SQLServerConnectionString)) {
-                using (var rModel = new RedisModel(RedisConnectionString)) {
-                    var rResult = await rModel.Get<ProfileResponseItem>($"{R_KEY}{PlayerGUID}");
+            using (var profileModel = new ProfileModel(SQLServerConnectionString)) {                
+                var rResult = await GetRedisObject<ProfileResponseItem>(PlayerGUID);
 
-                    if (!rResult.HasError) {
-                        return rResult;
-                    }
-
-                    var result = await profileModel.Profiles.FirstOrDefaultAsync(a => a.PlayerGUID == PlayerGUID);
-
-                    if (result == null) {
-                        throw new Exception($"PlayerGUID ({PlayerGUID} is null");
-                    }
-
-                    var response = new ProfileResponseItem {
-                        Alias = result.Alias,
-                        RegistrationDate = result.CreatedDate.DateTime
-                    };
-
-                    await rModel.Write($"{R_KEY}{PlayerGUID}", response);
-
-                    return new ReturnSet<ProfileResponseItem>(response);
+                if (!rResult.HasError) {
+                    return rResult;
                 }
+
+                var result = await profileModel.Profiles.FirstOrDefaultAsync(a => a.PlayerGUID == PlayerGUID);
+
+                if (result == null) {
+                    throw new Exception($"PlayerGUID ({PlayerGUID} is null");
+                }
+
+                var response = new ProfileResponseItem {
+                    Alias = result.Alias,
+                    RegistrationDate = result.CreatedDate.DateTime
+                };
+
+                await WriteRedisObject(PlayerGUID, response);
+
+                return new ReturnSet<ProfileResponseItem>(response);                
             }
         }
+
+        public override string GetRedisPrefix() => "PROFILE";
     }
 }
