be08a0b946768a0eff9d2f61e7605a54f79f40c5
Jarred Capellman <jcapellman@hotmail.com>
2016-12-04 21:02:07 -0500

    Added PlayerProfileHandler

---
+++ Src/MassiveAttack.Common.Library/WebAPIHandlers/PlayerProfileHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using MassiveAttack.Common.Library.Objects.Common;
+using MassiveAttack.Common.Library.Objects.WebAPI.Profiles;
+
+namespace MassiveAttack.Common.Library.WebAPIHandlers {
+    public class PlayerProfileHandler : BaseWebAPIHandler {
+        public PlayerProfileHandler(string webapiAddress, string token = null) : base(webapiAddress, token) { }
+
+        public async Task<ReturnSet<ProfileResponseItem>> GetProfile(Guid playerGUID)
+            => await GetAsync<ReturnSet<ProfileResponseItem>>($"Profile?PlayerGUID={playerGUID}");
+    }
+}
