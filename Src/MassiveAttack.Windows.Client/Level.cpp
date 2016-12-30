b5d050ca8de9a55ed01f2c4c2df04ea23b98b299
Jarred Capellman <jcapellman@hotmail.com>
2016-12-29 22:48:26 -0600

    Added: Ceiling, Floor and 2 walls in test level

--- Src/MassiveAttack.Windows.Client/Level.cpp
+++ Src/MassiveAttack.Windows.Client/Level.cpp
@@ -8,33 +8,69 @@ GLuint Level::LoadLevel(char* fileName)
 	glColor3f(1.0f, 1.0f, 1.0f);
 
 	glBegin(GL_TRIANGLES);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-1.0f, -1.0f, 1.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(1.0f, -1.0f, 1.0f);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(-1.0f, -1.0f, 1.0f);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, 1.0f);
 
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(1.0f, -1.0f, 1.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(1.0f, -1.0f, -1.0f);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, 1.0f);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(1.0f, -1.0f, -1.0f);
 
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(1.0f, -1.0f, -1.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(-1.0f, -1.0f, -1.0f);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(1.0f, -1.0f, -1.0f);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(-1.0f, -1.0f, -1.0f);
 
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(-1.0f, -1.0f, -1.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-1.0f, -1.0f, 1.0f);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(-1.0f, -1.0f, -1.0f);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(-1.0f, -1.0f, 1.0f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glColor3f(0.1f, 0.1f, 0.1f);
+
+		glVertex3f(100.0f, -1.0f, 100.0f);
+		glVertex3f(100.0f, -1.0f, 0.0f);
+		glVertex3f(0.0f, -1.0f, 0.0f);
+		glVertex3f(0.0f, -1.0f, 100.0f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glColor3f(0.4f, 0.4f, 0.4f);
+
+		glVertex3f(100.0f, 5.0f, 100.0f);
+		glVertex3f(100.0f, 5.0f, 0.0f);
+		glVertex3f(0.0f, 5.0f, 0.0f);
+		glVertex3f(0.0f, 5.0f, 100.0f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glColor3f(1.0f, 0.6f, 0.6f);
+
+		glVertex3f(100.0f, 5.0f, 100.0f);
+		glVertex3f(100.0f, -1.0f, 100.0f);
+		glVertex3f(0.0f, -1.0f, 100.0f);
+		glVertex3f(0.0f, 5.0f, 100.0f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glColor3f(0.6f, 1.0f, 0.6f);
+
+		glVertex3f(100.0f, 5.0f, 0.0f);
+		glVertex3f(100.0f, -1.0f, 0.0f);
+		glVertex3f(0.0f, -1.0f, 0.0f);
+		glVertex3f(0.0f, 5.0f, 0.0f);
 	glEnd();
 
 	glEndList();
