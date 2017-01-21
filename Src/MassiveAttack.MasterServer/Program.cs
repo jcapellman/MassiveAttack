86b8d829d44aaa7097bcf60e1e45eb73bc7be712
Jarred Capellman <jcapellman@hotmail.com>
2017-01-20 20:51:58 -0600

    Removed extraneous files

--- Src/MassiveAttack.MasterServer/Program.cs
+++ Src/MassiveAttack.MasterServer/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Hosting;
 
-namespace WebApplication
+namespace MassiveAttack.MasterServer
 {
     public class Program
     {
