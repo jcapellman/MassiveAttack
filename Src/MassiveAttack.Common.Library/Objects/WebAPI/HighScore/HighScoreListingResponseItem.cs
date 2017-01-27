4e414e942a00f07abacdc3cd428d8bd8ce72a517
Jarred Capellman <jcapellman@hotmail.com>
2017-01-26 21:55:07 -0600

    Switched the PCL to a .NET Standard Library

--- Src/MassiveAttack.Common.Library/Objects/WebAPI/HighScore/HighScoreListingResponseItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MassiveAttack.Common.Library.Objects.WebAPI.HighScore {
-
-    public class HighScoreListingResponseItem : BaseListingResponseItem {
-        public string PlayerName { get; set; }
-        
-        public int Score { get; set; }
-    }
-}
