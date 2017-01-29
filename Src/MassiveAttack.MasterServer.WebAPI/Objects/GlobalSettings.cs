848c468140050b8afc9222af7a3aa2607638d208
Jarred Capellman <jcapellman@hotmail.com>
2017-01-28 22:13:28 -0600

    Removed WebAPI MasterServer project

--- Src/MassiveAttack.MasterServer.WebAPI/Objects/GlobalSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MassiveAttack.MasterServer.WebAPI.Objects
-{
-    public class GlobalSettings
-    {
-        public string DBConnectionString { get; set; }
-
-        public string GameServerListImplementation { get; set; }
-    }
-}
