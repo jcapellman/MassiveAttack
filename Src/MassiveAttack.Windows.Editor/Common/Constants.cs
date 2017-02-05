5b5694db5808d18b98833f111e0df3a9219a0cec
Jarred Capellman <jcapellman@hotmail.com>
2017-02-05 15:40:53 -0600

    Removed older WebAPI and Library Items

--- Src/MassiveAttack.Windows.Editor/Common/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MassiveAttack.Windows.Editor.Common
-{
-    public static class Constants
-    {
-        public const string TEXTURE_DB_NAME = "base/texture.db";
-
-        public const string TEXTURE_PATH = "base\\textures\\";
-    }
-}
